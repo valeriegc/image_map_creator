bfc24a1c3966fa4a97679c4186bb611f4d7aadd8
Valerie Knape <valerieknape@gmail.com>
2023-05-07 15:17:53 +0300

    removed unnecessary syntax from canvas

--- src/routes/Canvas.svelte
+++ src/routes/Canvas.svelte
@@ -52,7 +52,7 @@
 			{#each $mapObjects as _, i}
 				<div
 					class="areaRect"
-					use:resizeArea={$mapObjects}
+					use:resizeArea
 					on:mousedown|stopPropagation={(e) => svgMoveStart(e, i)}
 					on:mousemove={svgMoveExecute}
 					on:mouseup={svgMoveEnd}
